d33ce04e7d250481fbf39e5f96d129b533f15913
Pritam Kumar <77887792+pritamkr1@users.noreply.github.com>
2021-03-04 00:04:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,89 @@
+let question = [{
+    quiz: 'What is your name?',
+    aOption: 'Pritam Kumar',
+    bOption: 'Shubham Kumar',
+    cOption: 'Rahul Kumar',
+    dOption: 'Raju Kumar',
+}, {
+    quiz: 'where are you from',
+    aOption: 'Hazaribagh',
+    bOption: 'Ranchi',
+    cOption: 'Bokaro',
+    dOption: 'Dhanbaad',
+},{
+    quiz: 'what is your hobby',
+    aOption: 'Playing',
+    bOption: 'Coding',
+    cOption: 'reading',
+    dOption: 'Swimming',
+}, {
+    quiz: 'favourite place',
+    aOption: 'Home',
+    bOption: 'office',
+    cOption: 'Garden',
+    dOption: 'School',
+}];
+
+//selecting item
+
+const questionName = document.querySelector('.question-head');
+const questionA = document.querySelector('#a-option');
+const questionB = document.querySelector('#b-option');
+const questionC = document.querySelector('#c-option');
+const questionD = document.querySelector('#d-option');
+const message = document.querySelector('.done');
+const btnSubmit = document.querySelector('.btn-check');
+const checkClass = document.querySelectorAll('.anwser');
+
+let currentQuiz = 0;
+
+let yourScore = 0;
+
+function gotSelected(){
+    let answer = undefined;
+    checkClass.forEach(function(eachClass){
+        
+       if(eachClass.checked){
+         return yourOption = eachClass.id;
+       }
+    });
+    return answer;
+}
+
+function deSelected(){
+    checkClass.forEach(function(eachClass){
+
+        eachClass.checked = false;
+
+    });
+}
+
+
+
+function showQuiz(){
+    deSelected();
+    let firstQuiz = question[currentQuiz];
+    questionName.innerHTML = firstQuiz.quiz;
+    questionA.textContent = firstQuiz.aOption;
+    questionB.textContent = firstQuiz.bOption;
+    questionC.textContent = firstQuiz.cOption;
+    questionD.textContent = firstQuiz.dOption;
+
+}
+
+showQuiz();
+
+btnSubmit.addEventListener('click', function(){
+    gotSelected();
+
+        currentQuiz++;
+        if(currentQuiz < question.length){
+            showQuiz();
+        }else{
+            var showMessage = `you are done!! and your score is ${yourScore}`;
+            message.textContent = showMessage;
+        }
+    
+    
+   
+});
